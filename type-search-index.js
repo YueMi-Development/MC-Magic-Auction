ea2976995fd960abfad2c9a7d9f823e9f2de483f
NekoMonci12 <muhammad.tamir@gmail.com>
2026-07-20 13:03:12 +0000

    Deploying to gh-pages from @ YueMi-Development/MC-Magic-Auction@90e0f2616a1685e393c6ee5071427104d8f6e08a 🚀

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.yuemi.magicauction.api","l":"MagicAuctionApi"},{"p":"org.yuemi.magicauction.api","l":"MagicAuctionApiProvider"}];updateSearchResults();
