96d8f78754c765d448bab277b62ef6f7e3c79afc
Muhammad Tamir <muhammad.tamir@gmail.com>
2026-07-20 12:11:01 +0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+subprojects {
+    apply(plugin = "java")
+
+    group = property("group") as String
+    version = property("version") as String
+
+    // Configure java extension safely
+    extensions.configure<JavaPluginExtension> {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(21))
+        }
+    }
+
+    tasks.withType<JavaCompile>().configureEach {
+        options.encoding = "UTF-8"
+    }
+
+    repositories {
+        mavenCentral()
+        maven("https://repo.papermc.io/repository/maven-public/")
+        maven("https://repo.yuemi.my.id/repository/maven-releases/")
+    }
+}
---
+++ core-plugin/build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    java
+    id("com.gradleup.shadow") version "8.3.0" // Shadow plugin
+}
+
+val pluginName: String by project
+val repoUrl: String by project
+val developerId: String by project
+val developerName: String by project
+val apiVersion: String by project
+val authors: String by project
+val contributors: String by project
+val pluginVersion: String = project.version.toString()
+
+tasks.processResources {
+    val bstatsPluginId = project.findProperty("bstatsPluginId") as? String ?: ""
+    val props = mapOf(
+        "pluginName" to pluginName,
+        "version" to pluginVersion,
+        "apiVersion" to apiVersion,
+        "authors" to authors,
+        "contributors" to contributors,
+        "bstatsPluginId" to bstatsPluginId
+    )
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("plugin.yml") {
+        expand(props)
+    }
+}
+
+
+dependencies {
+    implementation("org.bstats:bstats-bukkit:3.2.1")
+    implementation("org.yuemi:mc-config-libs:1.0.1")
+    implementation(project(":core-api"))
+    compileOnly("io.papermc.paper:paper-api:1.21.6-R0.1-SNAPSHOT")
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+}
+
+tasks.jar {
+    enabled = false
+}
+
+tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+    archiveBaseName.set(pluginName)
+    archiveVersion.set(pluginVersion)
+    archiveClassifier.set("")
+
+    configurations = listOf(project.configurations.runtimeClasspath.get())
+
+    relocate("org.bstats", "${project.group}.libs.bstats")
+    relocate("org.yuemi.config", "${project.group}.libs.config")
+
+    manifest {
+        attributes(
+            "Implementation-Title" to pluginName,
+            "Implementation-Version" to pluginVersion,
+            "Implementation-Vendor" to developerName,
+            "License" to "MIT"
+        )
+    }
+}
+
+tasks.build {
+    dependsOn(tasks.named("shadowJar"))
+}
